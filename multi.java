1eeacda8ee0a74496a496d89f5749238ec374200
dtc0823030 <dtc0823030@nttf.co.in>
2025-08-08 20:38:15 +0530

    Add files via upload

---
+++ multi.java
@@ -0,0 +1,10 @@
+public class multi {
+    public static void main(String[] args) {
+        
+        System.out.println("Multiplication of 3:");
+        
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("3 x " + i + " = " + (3 * i));
+        }
+    }
+}
